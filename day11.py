6990ed7cd02e0138c3feab05b9871de47c2eb0a4
jeroenweber <jeroen.weber@hu.nl>
2021-12-02 11:51:43 +0100

    generelized executeqyery

--- day11.py
+++ day11.py
@@ -1,4 +1,15 @@
 from readfromfile import readfromfile
 
-data = readfromfile('test.txt')
-print(data)
+data = readfromfile('puzzle11.txt')
+prev = int(data[0])
+data.pop(0)
+increases = 0
+total = 1
+for depth in data:
+    depth = int(depth)
+    if depth > prev:
+        increases += 1
+    prev = depth
+    total += 1
+print("increases\t" + str(increases))
+print("total\t\t" + str(total))
