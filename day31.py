3eeca8349383421e9b0688528d14b5f6653c63b2
jeroenweber <jeroen.weber@hu.nl>
2021-12-03 12:36:05 +0100

    generelized executeqyery

---
+++ day31.py
@@ -0,0 +1,34 @@
+from readfromfile import readfromfile
+
+data = readfromfile('power31.txt')
+
+gammarate = []
+epsilonrate = []
+rowlength = len(data[0])
+gammadecimal = 0
+epsilondecimal = 0
+
+#collect status to the length of row, positive is 0, negative is 1
+for i in range(rowlength):
+    gammarate.append(0)
+    epsilonrate.append(0)
+
+#O(rows*columns)
+for power in data:
+    for col in range(rowlength):
+        if (int(power[col]) == 0):
+            gammarate[col] += 1
+        else:
+            gammarate[col] -= 1
+
+for i in range(rowlength):
+    #1 wins for gamma, 0 no contribution to epsilon
+    if (gammarate[i] < 0):
+        gammadecimal += pow(2,(rowlength - i - 1))
+    #1 wins for epsilon, 0 no contribution to gamma
+    else:
+        epsilondecimal += pow(2,(rowlength - i - 1))
+
+print(gammadecimal)
+print(epsilondecimal)
+print(gammadecimal*epsilondecimal)
